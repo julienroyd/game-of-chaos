7ef9e28b3a9129636ff91914b069008d0343fe1d
julienroyd <jul.roy1311@gmail.com>
2021-06-22 16:41:08 -0400

    Changed average rule a bit

--- chaos.py
+++ chaos.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def sample_point(init_points, last_point):
+def sample_point(init_points, last_point, ref_weight=0.5):
     ref_point = init_points[np.random.randint(low=0, high=len(init_points))]
-    new_point = np.mean([ref_point, last_point], axis=0)
+    new_point = ref_weight * ref_point + (1 - ref_weight) * last_point
     return new_point
